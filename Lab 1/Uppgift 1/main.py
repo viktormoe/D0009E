118cef50fed820532d82a606c323f53c982cb9aa
viktormoe <viktor@moe.se>
2025-09-01 17:19:11 +0200

    Uppgift 1

---
+++ Lab 1/Uppgift 1/main.py
@@ -0,0 +1,25 @@
+
+# Lånade Beloppet
+P = 20000
+
+# Antal år för återbetalning
+a = 3
+
+# Årliga räntesatsen
+r = 0.02
+
+# Totala kostnaden för hela lånet
+k = P + (a+1) * P * r / 2
+
+print(k)
+
+
+
+m = 0
+
+print("Enter m value: ")
+
+m = input()
+print(" ")
+
+print("Väredet är ", m)
