9d65d07012ceea0863958525c20a03ed181dbe8b
viktormoe <viktor@moe.se>
2025-09-01 20:13:27 +0200

    Real done

--- Lab 1/Uppgift 1/test1.py
+++ Lab 1/Uppgift 1/test1.py
@@ -2,8 +2,4 @@ def kostnad(P, r, a):
     k = P + (a + 1) * P * r / 2
     print("Den totala kostnaden efter", a, "år är", int(k), "kr.")
 
-P = float(input("Ange lånebelopp: "))
-r = float(input("Ange ränta (t.ex. 0.03 för 3%): "))
-a = int(input("Ange antal år: "))
-
-kostnad(P, r, a)
+kostnad(50000, 0.03, 10)
