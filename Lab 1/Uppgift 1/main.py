88b51bbc2d5db56f47cdeb12fc302f33132c9d16
viktormoe <viktor@moe.se>
2025-09-01 18:03:15 +0200

    Hampus förbättring

--- Lab 1/Uppgift 1/main.py
+++ Lab 1/Uppgift 1/main.py
@@ -1,26 +1,11 @@
 
-# Lånade Beloppet
-P = 20000
+def tot_kos(P, a, r):
 
-# Antal år för återbetalning
-a = 3
+    # Totala kostnaden för hela lånet
+    k = P + (a+1) * P * r / 2
 
-# Årliga räntesatsen
-r = 0.02
-
-# Totala kostnaden för hela lånet
-k = P + (a+1) * P * r / 2
-
-print(k)
+    print("Det totala beloppet: ", k)
 
 
-
-m = 0
-
-print("Enter m value: ")
-
-m = input()
-print(" ")
-
-print("Väredet är ", m)
+tot_kos()
 
