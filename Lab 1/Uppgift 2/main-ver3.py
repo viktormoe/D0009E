3c31aa9fde5441a98f52d55be86d3a43dbcd3ede
viktormoe <viktor@moe.se>
2025-09-02 14:54:22 +0200

    fix

--- Lab 1/Uppgift 2/main-ver3.py
+++ Lab 1/Uppgift 2/main-ver3.py
@@ -1,3 +1,4 @@
+
 # 1 Lista på ingredienser för antal personer
 def recept(antal):
     print("smör:", (15/4)*antal)
@@ -10,17 +11,20 @@ def recept(antal):
     print("smör:", (75/4)*antal)
     print("vatten:", (1/4)*antal)
 
+
 # 2.1 Tid att blanda ingredienserna är 10 minuter plus 1 minuter per person
 def tidblanda(antal):
     tid = 10 + (1 * antal)
     print("\n")
     print("Total tid att baka kakan är", tid, "minuter")
 
+
 # 2.2 Tid att grädda kakan är 40 minuter plus 2 minuter per person
 def tidgradda(antal):
     tid = 40 + (2 * antal)
     print("\n")
     print("Total tid att grädda kakan är", tid, "minuter")
+
 
 # 3 Sammanlagd tid att baka kakan
 def sockerkaka(antal):
@@ -30,6 +34,7 @@ def sockerkaka(antal):
     tidblanda(antal)
     tidgradda(antal)
     print("Totalt tar det", (10 + 1 * antal) + (40 + 2 * antal), "minuter att baka sockerkakan för", antal, "personer.")
+
 
 # Huvudprogram (script) som skriver ut recept för 4 och 7 personer
 sockerkaka(4)
