81e627d75c256607482492dce5e96db88bfa36be
viktormoe <viktor@moe.se>
2025-09-02 11:33:43 +0200

    Uppgift 2 Klar

--- Lab 1/Uppgift 2/main.py
+++ Lab 1/Uppgift 2/main.py
@@ -1,5 +1,24 @@
 def recept(antal):
-        
+    bas = {
+        "smör, g": 15,
+        "ströbröd, msk": 3,
+        "ägg, st": 3,
+        "strösocker, dl": 3,
+        "vaniljsocker, tsk": 2,
+        "bakpulver, tsk": 2,
+        "vetemjöl, dl": 3,
+        "smör, g": 75,
+        "vatten, dl": 1
+
+    }
+
+    # Uträkning för hur mycket ingridienser för x antal personer
+    faktor = antal / 4
+
+    print("\n")
+
+    print((faktor*antal)*bas)
 
 
+# Hur många ska kakan räcka till
 recept(4)
---
+++ Lab 1/Uppgift 2/test1.py
@@ -0,0 +1,13 @@
+def recept(antal):
+    print("smör:", (15/4)*antal)
+    print("ströbröd:", (3/4)*antal)
+    print("ägg:", (int((3/4)*antal)))
+    print("strösocker:", (3/4)*antal)
+    print("vaniljsocker:", (2/4)*antal)
+    print("bakpulver:", (2/4)*antal)
+    print("vetemjöl:", (3/4)*antal)
+    print("smör:", (75/4)*antal)
+    print("vatten:", (1/4)*antal)
+
+
+recept(4)
