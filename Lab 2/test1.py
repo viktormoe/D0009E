4acaf6b2ebb19b18df9bc9625dd0a4b4529673c1
viktormoe <viktor@moe.se>
2025-09-08 20:32:50 +0200

    upd

--- Lab 2/test1.py
+++ Lab 2/test1.py
@@ -1,4 +1,4 @@
-# 1 
+# 1 Rekursiv
 def bounce(n):
     print(n)
     if n>0:
@@ -7,4 +7,26 @@ def bounce(n):
 
 bounce(4)
 
-# 2
+print("\n"*2)
+
+
+# 2 Iterativ
+def bounce2(n):
+    for i in range(n,-1,-1):
+        print(i)
+    for i in range(1,n+1):
+        print(i)
+
+
+bounce2(5)
+
+print("\n"*2)
+
+
+# 3 Rekursiv
+def tvarsumma(n):
+    print(n)
+    if 
+
+
+tvarsumma(123456789)
