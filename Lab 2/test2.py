ccd5aed8e78681c52498c1211fb648fdac243669
viktormoe <viktor@moe.se>
2025-09-10 11:39:50 +0200

    upd

--- Lab 2/test2.py
+++ Lab 2/test2.py
@@ -1,5 +1,5 @@
 
-print("Rekursiv")
+print("\nRekursiv:")
 
 def bounce(n):
     if  (n > 0):
@@ -7,5 +7,39 @@ def bounce(n):
         bounce(n - 1)
     print(n)
 
-
 bounce(5)
+
+print("\n\nIterativ:")
+
+def bounce2(n):
+    for i in range(n,0,-1):
+        print(i)
+
+    for i in range(0,n+1,1):
+        print(i) 
+
+bounce2(5)
+
+print("\n")
+
+bounce(0)
+
+print("\n")
+
+bounce2(0)
+
+
+print("\n\nIterativ:")
+
+def bounce3(n):
+    i = n
+    while i > 0:   # same as for i in range(n,0,-1)
+        print(i)
+        i -= 1
+
+    i = 0
+    while i <= n:  # same as for i in range(0,n+1,1)
+        print(i)
+        i += 1
+
+bounce3(6)
