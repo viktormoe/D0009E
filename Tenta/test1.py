d79aee12346d869bbb930ec4d58fe50b0a6decb9
viktormoe <viktor@moe.se>
2025-10-06 20:32:21 +0200

    upd

---
+++ Tenta/test1.py
@@ -0,0 +1,12 @@
+Ages = [55, 15, 20, 21, 99, 60, 30, 11]
+
+for age in Ages:
+    if (age < 18):
+        print("Child")
+
+    elif (64 >= age >= 18):
+        print("Adult")
+    
+    elif (age > 64):
+        print("Boomer")
+
