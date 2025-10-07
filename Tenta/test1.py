6062cb2c25e191c48cbfbee48dc378a39d2f5abd
viktormoe <viktor@moe.se>
2025-10-07 17:08:54 +0200

    upd

--- Tenta/test1.py
+++ Tenta/test1.py
@@ -1,12 +1,4 @@
-Ages = [55, 15, 20, 21, 99, 60, 30, 11]
+namn = ["Anna", "Björn", "Calle"]
 
-for age in Ages:
-    if (age < 18):
-        print("Child")
-
-    elif (64 >= age >= 18):
-        print("Adult")
-    
-    elif (age > 64):
-        print("Boomer")
-
+for n in namn:
+    print("Hej", n)
