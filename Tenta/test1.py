b0cad5a0ced1bf2fc817513fbb4622ad408654e5
viktormoe <viktor@moe.se>
2025-10-12 18:52:25 +0200

    upd

--- Tenta/test1.py
+++ Tenta/test1.py
@@ -1,4 +1,8 @@
-namn = ["Anna", "Björn", "Calle"]
-
-for n in namn:
-    print("Hej", n)
+s="skruvmejsel"
+n=len(s)
+if not len(s)>=0:
+ print("mutter")
+elif len(s[:3])>n or s[0]==s[8]:
+ print("skruv")
+else:
+ print("bricka")
