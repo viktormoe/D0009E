b63812a19b6f1b4db0cac644c281912aaaa068d5
viktormoe <viktor@moe.se>
2025-10-16 10:31:48 +0200

    upd

--- Tenta/Tenta1.py
+++ Tenta/Tenta1.py
@@ -1,14 +1,9 @@
-"""Someone has written a function, f, that takes a list, lst, and a value e as arguments.
-Function should count and return the number of occurrences of e in lst and return the
-result.
-The programmer arrived at the following:"""
-def f(lst, e):
-    num=0
-    for c in lst:
-        if c==e:
-            num +=1
-            
-    print(num)
-"""The programmer made mistakes. The task is to write a corrected version. It is not allowed
-to write an entirely new program; only the errors in this existing code should be corrected.
-Changing the loop type is also not allowed (from while to for or vice versa)."""
+def replaceAll1(lst, element, replace):
+    i=0
+    while i<len(lst):
+        if lst[i]==element:
+            lst[i]=replace
+        i=i+1
+    return lst
+lst1=[4,1,0,1]
+lstA1=replaceAll1(lst1,1,9)
