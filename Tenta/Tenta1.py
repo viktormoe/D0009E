e466ec7d51769974341be61c1feff1b5cd41d584
viktormoe <viktor@moe.se>
2025-10-16 13:04:01 +0200

    upd

--- Tenta/Tenta1.py
+++ Tenta/Tenta1.py
@@ -1,9 +1,8 @@
-def replaceAll1(lst, element, replace):
-    i=0
-    while i<len(lst):
-        if lst[i]==element:
-            lst[i]=replace
-        i=i+1
-    return lst
-lst1=[4,1,0,1]
-lstA1=replaceAll1(lst1,1,9)
+def print_chars(s):
+    a = 0
+    for x in s[a]:
+        print(x)
+        a =+ 1
+
+
+print_chars("hej")
