0da9b5b57215166052ad3ea2ae9c2d9f96c4eb02
viktormoe <viktor@moe.se>
2025-10-21 13:52:43 +0200

    upd

--- Tenta/Tenta1.py
+++ Tenta/Tenta1.py
@@ -1,17 +1,9 @@
-Someone has written a function, frequency, that takes a list of numbers, lst, and a 
-value x as arguments. Function should count and return the number of occurrences of x in 
-lst.
-Programmeraren kom fram till följande:
-def frequency(lst, x):
-    count=0
-    i=0
-    while i< len(lst):
-        if lst[i]==x:
-            count += 1
-            i += 1
-        else:
-            i+=1
-    print(count)
-The programmer made mistakes. The task is to write a corrected version. It is not allowed 
-to write an entirely new program; only the errors in this existing code should be corrected. 
-Changing the loop type is also not allowed (from while to for or vice versa).
+def readFloat():
+    f=open("testfil","r")
+    value = float(f.readline())
+    f.close()
+    return value
+
+def readUI():
+    value = readFloat()
+    print(value)
